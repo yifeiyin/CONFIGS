95e3f857f271f9fb67292d122aacbe428cf1acfe
Yifei Yin <yifeiyin@foxmail.com>
2020-04-10 01:36:09 -0400

    Update readme.sh

--- git/readme.sh
+++ git/readme.sh
@@ -1,4 +1,4 @@
 git config --global alias.s status
 git config --global alias.br branch
-git config --global alias.cm commit -m
+git config --global alias.cm "commit -m"
 git config --global alias.co checkout
